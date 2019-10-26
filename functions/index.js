17cbbf3648e5ac8949317c6da9f763c7dfa867a8
DeAndre Johnson <dre@phreshr.com>
2019-10-26 13:20:07 +0800

    init firebase functions

---
+++ functions/index.js
@@ -0,0 +1,61 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
+
+// On sign up.
+exports.processSignUp = functions.auth.user().onCreate(async user => {
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'user',
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-user-id': user.uid,
+    },
+  }
+
+  try {
+    const setCustomUserClaims = admin
+      .auth()
+      .setCustomUserClaims(user.uid, customClaims)
+
+    const metadata = admin
+      .firestore()
+      .collection('metadata')
+      .doc(user.uid)
+      .set({ refreshTime: new Date().getTime() })
+
+    return await Promise.all([setCustomUserClaims, metadata])
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// On sign up.
+exports.processUser = functions.auth.user().onCreate(user => {
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'user',
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-user-id': user.uid,
+    },
+  }
+
+  // Set custom user claims on this newly created user.
+  return admin
+    .auth()
+    .setCustomUserClaims(user.uid, customClaims)
+    .then(() => {
+      // Update real-time database to notify client to force refresh.
+      // const metadataRef = admin.database().ref('metadata/' + user.uid)
+      // Set the refresh time to the current UTC timestamp.
+      // This will be captured on the client to force a token refresh.
+      // return metadataRef.set({ refreshTime: new Date().getTime() })
+      return admin
+        .firestore()
+        .collection('metadata')
+        .doc(user.uid)
+        .set({ refreshTime: new Date().getTime() })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
