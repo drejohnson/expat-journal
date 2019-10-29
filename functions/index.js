3449069b25a5d85e09f2c0593ef272aba1d266a0
DeAndre Johnson <dre@phreshr.com>
2019-10-29 13:47:28 +0800

    update for firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,16 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const request = require('graphql-request')
+
 admin.initializeApp(functions.config().firebase)
+
+// TODO: Add Environment variables
+const client = new request.GraphQLClient(process.env.GRAPHQL_ENDPOINT, {
+  headers: {
+    'content-type': 'application/json',
+    'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
+  },
+})
 
 // On sign up.
 exports.processSignUp = functions.auth.user().onCreate(async user => {
@@ -27,35 +37,4 @@ exports.processSignUp = functions.auth.user().onCreate(async user => {
   } catch (error) {
     console.log(error)
   }
-})
-
-// On sign up.
-exports.processUser = functions.auth.user().onCreate(user => {
-  const customClaims = {
-    'https://hasura.io/jwt/claims': {
-      'x-hasura-default-role': 'user',
-      'x-hasura-allowed-roles': ['user'],
-      'x-hasura-user-id': user.uid,
-    },
-  }
-
-  // Set custom user claims on this newly created user.
-  return admin
-    .auth()
-    .setCustomUserClaims(user.uid, customClaims)
-    .then(() => {
-      // Update real-time database to notify client to force refresh.
-      // const metadataRef = admin.database().ref('metadata/' + user.uid)
-      // Set the refresh time to the current UTC timestamp.
-      // This will be captured on the client to force a token refresh.
-      // return metadataRef.set({ refreshTime: new Date().getTime() })
-      return admin
-        .firestore()
-        .collection('metadata')
-        .doc(user.uid)
-        .set({ refreshTime: new Date().getTime() })
-    })
-    .catch(error => {
-      console.log(error)
-    })
 })
