3354db722a50a3ad96f386b1e39600c3e7d1d091
DeAndre Johnson <dre@phreshr.com>
2019-11-06 02:14:27 +0800

    add providers to root level component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
 import { h } from 'preact'
-import { Switch, Route } from 'wouter-preact'
+import { Switch, Route, Redirect } from 'wouter-preact'
 
-import { AuthProvider } from './context/Auth'
-import { FirebaseProvider } from './context/Firebase'
-import ApolloProvider from './apollo/ApolloProvider'
+import useAuth from './hooks/useAuth'
 
 // Code-splitting is automated for routes
 import PrivateRoute from './components/PrivateRoute'
 import Home from './routes/Home'
 import Posts from './routes/Posts'
 import Login from './routes/Login'
-import Register from './routes/Register'
 
 const App = () => {
+  const { isAuthenticated } = useAuth()
   return (
-    <FirebaseProvider>
-      <AuthProvider>
-        <ApolloProvider>
-          <div id="app">
-            <Switch>
-              <PrivateRoute path="/">
-                <Home />
-              </PrivateRoute>
-              <PrivateRoute path="/posts">
-                <Posts />
-              </PrivateRoute>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/register">
-                <Register />
-              </Route>
-              <Route path="/:rest">
-                <center>
-                  <b>404:</b> Sorry, this page isn't ready yet!
-                </center>
-              </Route>
-            </Switch>
-          </div>
-        </ApolloProvider>
-      </AuthProvider>
-    </FirebaseProvider>
+    <div id="app">
+      <Switch>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+        <PrivateRoute path="/posts">
+          <Posts />
+        </PrivateRoute>
+        <Route path="/login">
+          {isAuthenticated ? <Redirect to="/" /> : <Login />}
+        </Route>
+        <Route path="/:rest">
+          <center>
+            <b>404:</b> Sorry, this page isn't ready yet!
+          </center>
+        </Route>
+      </Switch>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import { h } from 'preact'
 import './style'
+
+import { AuthProvider } from './context/Auth'
+import { FirebaseProvider } from './context/Firebase'
+import ApolloProvider from './apollo/ApolloProvider'
+
 import App from './App'
 
-export default App
+export default () => {
+  return (
+    <FirebaseProvider>
+      <AuthProvider>
+        <ApolloProvider>
+          <App />
+        </ApolloProvider>
+      </AuthProvider>
+    </FirebaseProvider>
+  )
+}
